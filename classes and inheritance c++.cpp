3168530d373c8b09b5b699442881848778b0cd6f
jomo4jo@hotmail.com <jomo4jo@hotmail.com>
2020-10-18 20:28:09 -0500

    created customer class which inherits from person data

--- classes and inheritance c++.cpp
+++ classes and inheritance c++.cpp
@@ -32,7 +32,49 @@ mailingList
 The customerNumber variable will be used to hold a unique integer for each customer. The mailingList variable should be a bool. It will be set to true if the customer wishes to be on a mailing list, or false if the customer does not wish to be on a mailing list. Write appropriate accessor and mutator functions for these member variables. Demonstrate an object of the CustomerData class in a simple program.*/
 #include <iostream>
 using namespace std;
+
+class PersonData
+{
+public:
+	void setNames(string, string);
+	string getNames()const;
+
+	PersonData();
+	~PersonData();
+
+private:
+	string lastName;
+	string firstName;
+	string address[3];
+	string state;
+	string city;
+	string zip;
+	string phone;
+
+};
+
+PersonData::PersonData()
+{
+}
+
+PersonData::~PersonData()
+{
+}
+// customer class defination on 64
+class customer :public PersonData {
+private:
+	string lastName;
+	string firstName;
+	string address[3];
+	string state;
+	string city;
+	string zip;
+	string phone;
+public:
+	
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    return 0;
 }
